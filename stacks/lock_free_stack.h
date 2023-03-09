216bb585624702f040d0ac781832d89b857327c6
Rauf Shimarov <rshimaro@sfu.ca>
2023-03-08 21:40:22 -0800

    Added counter logic to lock-free stack

--- stacks/lock_free_stack.h
+++ stacks/lock_free_stack.h
@@ -1,18 +1,37 @@
 #include "../common/allocator.h"
 #include "../common/utils.h"
 
+#define LFENCE asm volatile("lfence" : : : "memory")
+#define SFENCE asm volatile("sfence" : : : "memory")
+
+template<class P>
+struct pointer_t {
+    P* ptr;
+
+    P* address(){
+        //Get the address by getting the 48 least significant bits of ptr
+        uintptr_t mask = (1ULL << 48) - 1;
+        return reinterpret_cast<P*>(reinterpret_cast<uintptr_t>(ptr) & mask);
+    }
+    uint count(){
+        //Get the count from the 16 most significant bits of ptr
+        uintptr_t mask = (1ULL << 16) - 1;
+        return static_cast<uint>((reinterpret_cast<uintptr_t>(ptr) >> 48) & mask);
+    }
+};
+
 template <class T>
 class Node
 {
 public:
     T value;
-    Node<T>* next;
+    pointer_t<Node<T>> next;
 };
 
 template <class T>
 class LockFreeStack
 {
-    Node<T>* top;
+    pointer_t<Node<T>> q_top;
     CustomAllocator my_allocator_;
 public:
     LockFreeStack() : my_allocator_()
@@ -26,62 +45,72 @@ public:
         my_allocator_.initialize(t_my_allocator_size, sizeof(Node<T>));
         // Perform any necessary initializations
         Node<T>* newNode = (Node<T>*)my_allocator_.newNode();
-        newNode->next = NULL;
-        top = newNode;
+        newNode->next.ptr = NULL;
+        q_top.ptr = newNode;
+    }
+
+    pointer_t<Node<T>> incrementAndCombine(Node<T>* ptr, uint count){
+        uintptr_t combined_ptr_int = (static_cast<uintptr_t>(count) << 48) | reinterpret_cast<uintptr_t>(ptr);
+        pointer_t<Node<T>> res;
+        Node<T>* node_ptr = reinterpret_cast<Node<T>*>(combined_ptr_int);
+        res.ptr = node_ptr;
+        return res;
     }
 
     bool tryPush(Node<T>* node){
-        Node<T>* oldTop = top;
-        node->next = oldTop;
-        return CAS(&top, oldTop, node);	
+        pointer_t<Node<T>> top = q_top;
+        LFENCE;
+        if (top.ptr == q_top.ptr ) {
+            node->next = top;
+        }
+        return CAS(&q_top, top, incrementAndCombine(node, top.count() + 1));	
     }
 
     /**
-     * Create a new node with value `value` and update it to be the top of the stack.
+     * Create a new node with value `value` and update it to be the q_top of the stack.
      * This operation must always succeed.
      */
     void push(T value)
     {
         Node<T>* node = (Node<T>*)my_allocator_.newNode();
         node->value = value;
+        SFENCE;
         while(true){
             if(tryPush(node)){
                 return;
             }
         }
-    }
-
-    Node<T>* tryPop()
-    {
-        Node<T>* oldTop = top;
-        Node<T>* newTop = top->next;
-        if(CAS(&top, oldTop, newTop)){
-            return oldTop;
-        } else {
-            return NULL;
-        }
+        // SFENCE;
     }
 
     /**
      * If the stack is empty: return false.
-     * Otherwise: copy the value at the top of the stack into `value`, update
-     * the top to point to the next element in the stack, and return true.
+     * Otherwise: copy the value at the q_top of the stack into `value`, update
+     * the q_top to point to the next element in the stack, and return true.
      */
     bool pop(T *value)
     {
-        Node<T>* returnNode;
+        pointer_t<Node<T>> top;
+        pointer_t<Node<T>> next;
         while(true){
-            if(top->next == NULL){
-                // Stack is empty
-                return false;
+            LFENCE;
+            top = q_top;
+            LFENCE;
+            next = top.address()->next;
+            LFENCE;
+            if (top.ptr == q_top.ptr) {
+                if(next.address() == NULL){
+                    // Stack is empty
+                    return false;
+                }
             }
-            returnNode = tryPop();
-            if (returnNode != NULL){
-                *value = returnNode->value;
-                my_allocator_.freeNode(returnNode);
-                return true;
-            }
+            *value = top.address()->value;
+            if(CAS(&q_top, top, incrementAndCombine(next.address(), top.count() + 1))){
+                break;
+            } 
         }
+        my_allocator_.freeNode(top.address());
+        return true;
     }
 
     void cleanup()
